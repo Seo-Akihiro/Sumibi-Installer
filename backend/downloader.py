0970e88dde64cc390c25fd2c09fb5417981cb3a2
MacG <kemutan0909@gmail.com>
2025-06-28 17:15:54 +0900

    動画をインストールできるように

--- backend/downloader.py
+++ backend/downloader.py
@@ -1,2 +1,13 @@
 import yt_dlp
-print("yt-dlp imported successfully!")
+
+URL = input("動画のURLを入力: ")
+
+print("ダウンロードを開始します...")
+
+opts = {
+        'outtmpl': '%(title)s.%(ext)s',  # ファイル名
+        'format': 'best',                # 品質
+        'nocheckcertificate': True,      # SSL証明書の検証をスキップ
+        }
+with yt_dlp.YoutubeDL(opts) as ydl:
+        ydl.download([URL])
